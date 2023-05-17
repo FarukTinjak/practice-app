88dda7f67321b776add9a61928f1f08fb0dce4f6
Faruk Tinjak <Fare_TC@hotmail.com>
2023-05-17 15:24:46 +0200

    Refactor JSX to React.createElement()

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Shoppings from "./components/Shoppings";
 
 function App() {
@@ -7,12 +8,18 @@ function App() {
     { id: "e3", name: "Coca-Cola", amount: 2.95, date: new Date(2023, 2, 23) },
     { id: "e4", name: "Lemon", amount: 5.12, date: new Date(2023, 1, 11) },
   ];
-  return (
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h2", {}, "Let's get started"),
+    React.createElement(Shoppings, { items: shoppings })
+  );
+  /* return (
     <div>
       <h2>Let's get started!</h2>
       <Shoppings items={shoppings} />
     </div>
-  );
+  ); */
 }
 
 export default App;
