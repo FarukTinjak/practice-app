0511c879f8e4f0f33da16269e3236513e8ad909b
Faruk Tinjak <Fare_TC@hotmail.com>
2023-05-17 22:11:26 +0200

    Add new item shoppingquantity

--- src/App.js
+++ src/App.js
@@ -3,23 +3,47 @@ import Shoppings from "./components/Shoppings/Shoppings";
 
 const App = () => {
   const shoppings = [
-    { id: "e1", name: "Milk", amount: 2.0, date: new Date(2023, 4, 16) },
-    { id: "e2", name: "Chocolate", amount: 1.67, date: new Date(2023, 4, 16) },
-    { id: "e3", name: "Coca-Cola", amount: 2.95, date: new Date(2023, 2, 23) },
-    { id: "e4", name: "Lemon", amount: 5.12, date: new Date(2023, 1, 11) },
+    {
+      id: "e1",
+      name: "Milk",
+      quantity: 7,
+      amount: 2.2434,
+      date: new Date(2023, 4, 16),
+    },
+    {
+      id: "e2",
+      name: "Chocolate",
+      quantity: 1,
+      amount: 1.67232,
+      date: new Date(2023, 4, 16),
+    },
+    {
+      id: "e3",
+      name: "Coca-Cola",
+      quantity: 3,
+      amount: 2.9325,
+      date: new Date(2023, 2, 23),
+    },
+    {
+      id: "e4",
+      name: "Lemon",
+      quantity: 12,
+      amount: 5.2312,
+      date: new Date(2023, 1, 11),
+    },
   ];
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h2", {}, "Let's get started"),
-    React.createElement(Shoppings, { items: shoppings })
-  );
-  /* return (
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Let's get started"),
+  //   React.createElement(Shoppings, { items: shoppings })
+  // );
+  return (
     <div>
       <h2>Let's get started!</h2>
       <Shoppings items={shoppings} />
     </div>
-  ); */
+  );
 };
 
 export default App;
