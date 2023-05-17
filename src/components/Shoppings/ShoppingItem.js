a9d092423a54dd45d4160ff6c0c48e69111c1f60
Faruk Tinjak <Fare_TC@hotmail.com>
2023-05-18 01:57:21 +0200

    Refactored component to use hooks for state management

--- src/components/Shoppings/ShoppingItem.js
+++ src/components/Shoppings/ShoppingItem.js
@@ -1,27 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ShoppingItem.css";
 import ShoppingDate from "./ShoppingDate";
 import Card from "../UI/Card";
 
 const ShoppingItem = (props) => {
-  let title = props.title;
-  const clickHandler = () => {
-    title = "Updated";
+  const [name, setName] = useState(props.name);
+  const [quantity, setQuantity] = useState(props.quantity);
+  const [amount, setAmount] = useState(props.amount);
+  const [date, setDate] = useState(props.date);
+  const editHandler = (event) => {
+    setName(event.target.value);
+  };
+  const incrementHandler = () => {
+    setQuantity(quantity + 1);
+  };
+  const decrementHandler = () => {
+    if (quantity >= 2) setQuantity(quantity - 1);
   };
 
   return (
     <Card className="shopping-item">
-      <ShoppingDate date={props.date} />
+      <ShoppingDate date={date} />
       <div className="shopping-item__name">
-        <h2>{props.name}</h2>
+        <input defaultValue={name} onChange={editHandler} />
+
         <div className="shopping-item__quantity_container">
-          <button className="counter counter-decrement">-</button>
-          <div className="shopping-item__quantity">{props.quantity}</div>
-          <button className="counter counter-increment">+</button>
+          <button
+            onClick={decrementHandler}
+            className="counter counter-decrement"
+          >
+            -
+          </button>
+          <div className="shopping-item__quantity">{quantity}</div>
+          <button
+            onClick={incrementHandler}
+            className="counter counter-increment"
+          >
+            +
+          </button>
         </div>
-        <div className="shopping-item__price">{props.amount.toFixed(2)}KM</div>
+        <div className="shopping-item__price">
+          {(amount * quantity).toFixed(2)}KM
+        </div>
       </div>
-      <button onClick={clickHandler}>Change name</button>
     </Card>
   );
 };
