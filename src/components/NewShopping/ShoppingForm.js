2ce402a8b982d0a5452e91385d39fb5bcee606ad
Faruk Tinjak <Fare_TC@hotmail.com>
2023-05-18 21:24:33 +0200

    Add input change handlers for  variables

--- src/components/NewShopping/ShoppingForm.js
+++ src/components/NewShopping/ShoppingForm.js
@@ -1,35 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ShoppingForm.css";
 const ShoppingForm = () => {
-  const nameChangeHandler = () => {};
-  const submitHandler = (event) => {
-    event.preventDefault();
-    console.log("ok");
+  //   const [enteredName, SetEndeteredName] = useState("");
+  //   const [enteredAmount, SetEndeteredAmount] = useState("");
+  //   const [enteredQuantity, SetEndeteredQuantity] = useState("");
+  //   const [enteredDate, SetEndeteredDate] = useState("");
+
+  const [userInput, setUserInput] = useState({
+    enteredName: "",
+    enteredAmount: "",
+    enteredQuantity: "",
+    enteredDate: "",
+  });
+
+  const nameChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      enteredName: event.target.value,
+    });
   };
-  return (
-    <form onSubmit={submitHandler}>
-      <div className="new-shopping__controls">
-        <div className="new-shopping__control">
-          <label>Name</label>
-          <input type="text" onChange={nameChangeHandler}></input>
-        </div>
-        <div className="new-shopping__control">
-          <label>Amount</label>
-          <input type="number" min="0.10" step="0.05" />
-        </div>
-        <div className="new-shopping__control">
-          <label>Quantity</label>
-          <input type="number" min="1" step="1" />
-        </div>
-        <div className="new-shopping__control">
-          <label>Date</label>
-          <input type="date" min="2023-1-1"></input>
-        </div>
-      </div>
-      <div className="new-shopping__actions">
-        <button type="submit">Add Shopping</button>
-      </div>
-    </form>
-  );
+  const amountChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value,
+    });
+  };
 };
+const quantityChangeHandler = (event) => {
+  setUserInput({
+    ...userInput,
+    enteredQuantity: event.target.value,
+  });
+};
+const dateChangeHandler = (event) => {
+  setUserInput({
+    ...userInput,
+    enteredDate: event.target.value,
+  });
+};
+
+const submitHandler = (event) => {
+  event.preventDefault();
+  console.log("ok");
+};
+return (
+  <form onSubmit={submitHandler}>
+    <div className="new-shopping__controls">
+      <div className="new-shopping__control">
+        <label>Name</label>
+        <input type="text" onChange={nameChangeHandler}></input>
+      </div>
+      <div className="new-shopping__control">
+        <label>Amount</label>
+        <input
+          type="number"
+          min="0.10"
+          step="0.05"
+          onChange={amountChangeHandler}
+        />
+      </div>
+      <div className="new-shopping__control">
+        <label>Quantity</label>
+        <input
+          type="number"
+          min="1"
+          step="1"
+          onChange={quantityChangeHandler}
+        />
+      </div>
+      <div className="new-shopping__control">
+        <label>Date</label>
+        <input type="date" min="2023-1-1" onChange={dateChangeHandler}></input>
+      </div>
+    </div>
+    <div className="new-shopping__actions">
+      <button type="submit">Add Shopping</button>
+    </div>
+  </form>
+);
+
 export default ShoppingForm;
