04d03b11d5e3f90b7b3318f14e30e28a1bcea862
Faruk Tinjak <Fare_TC@hotmail.com>
2023-05-20 21:43:23 +0200

    Edit Shoppings component with filter functionality

--- src/components/Shoppings/Shoppings.js
+++ src/components/Shoppings/Shoppings.js
@@ -1,36 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingItem from "./ShoppingItem";
 import Card from "../UI/Card";
 import "./Shoppings.css";
+import ShoppingsFilter from "./ShoppingsFilter";
 
 const Shoppings = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2023");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
-    <Card className="shoppings">
-      <ShoppingItem
-        name={props.items[0].name}
-        amount={props.items[0].amount}
-        quantity={props.items[0].quantity}
-        date={props.items[0].date}
-      />
-      <ShoppingItem
-        name={props.items[1].name}
-        amount={props.items[1].amount}
-        quantity={props.items[1].quantity}
-        date={props.items[1].date}
-      />
-      <ShoppingItem
-        name={props.items[2].name}
-        amount={props.items[2].amount}
-        quantity={props.items[2].quantity}
-        date={props.items[2].date}
-      />
-      <ShoppingItem
-        name={props.items[3].name}
-        amount={props.items[3].amount}
-        quantity={props.items[3].quantity}
-        date={props.items[3].date}
-      />
-    </Card>
+    <div>
+      <Card className="shoppings">
+        <ShoppingsFilter
+          selectedYear={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {props.items.map((shopping) => (
+          <ShoppingItem
+            key={shopping.id}
+            name={shopping.name}
+            amount={shopping.amount}
+            quantity={shopping.quantity}
+            date={shopping.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
 };
 export default Shoppings;
