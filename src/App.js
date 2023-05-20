fbbf19189bcff7020a6d0e5f29127bcb3096921a
Faruk Tinjak <Fare_TC@hotmail.com>
2023-05-20 21:46:43 +0200

    Add new shopping dynamically

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Shoppings from "./components/Shoppings/Shoppings";
 import NewShopping from "./components/NewShopping/NewShopping";
+const DUMMY_SHOPPINGS = [
+  {
+    id: "e1",
+    name: "Milk",
+    quantity: 7,
+    amount: 2.2434,
+    date: new Date(2023, 4, 16),
+  },
+  {
+    id: "e2",
+    name: "Chocolate",
+    quantity: 1,
+    amount: 1.67232,
+    date: new Date(2023, 4, 16),
+  },
+  {
+    id: "e3",
+    name: "Coca-Cola",
+    quantity: 3,
+    amount: 2.9325,
+    date: new Date(2023, 2, 23),
+  },
+  {
+    id: "e4",
+    name: "Lemon",
+    quantity: 12,
+    amount: 5.2312,
+    date: new Date(2023, 1, 11),
+  },
+];
+
 const App = () => {
-  const shoppings = [
-    {
-      id: "e1",
-      name: "Milk",
-      quantity: 7,
-      amount: 2.2434,
-      date: new Date(2023, 4, 16),
-    },
-    {
-      id: "e2",
-      name: "Chocolate",
-      quantity: 1,
-      amount: 1.67232,
-      date: new Date(2023, 4, 16),
-    },
-    {
-      id: "e3",
-      name: "Coca-Cola",
-      quantity: 3,
-      amount: 2.9325,
-      date: new Date(2023, 2, 23),
-    },
-    {
-      id: "e4",
-      name: "Lemon",
-      quantity: 12,
-      amount: 5.2312,
-      date: new Date(2023, 1, 11),
-    },
-  ];
+  const [shoppings, setShoppings] = useState(DUMMY_SHOPPINGS);
+
   const addShoppingHandler = (shopping) => {
-    console.log("In.App.js");
-    console.log(shopping);
+    setShoppings((prevShoppings) => {
+      return [shopping, ...prevShoppings];
+    });
   };
   // return React.createElement(
   //   "div",
@@ -49,5 +53,4 @@ const App = () => {
     </div>
   );
 };
-
 export default App;
